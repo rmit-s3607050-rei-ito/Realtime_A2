7384e51058ebbc3815c966a4e94ed5f06439d00d
PacificThai <s3429648@student.rmit.edu.au>
2017-09-16 16:37:31 +1000

    Fixed problem with tute 7

--- examples/7-redo/shader.vert
+++ examples/7-redo/shader.vert
@@ -1,24 +1,22 @@
 //shader.vert
+varying vec3 color;
 
 #define M_PI 3.1415926535897932384626433832795
 
-// uniform = values obtained from sinewave3D-glm.cpp file
-uniform mat3 nMat;          // normalMatrix
-uniform mat4 mvMat;         // modelViewMatrix
-uniform mat4 pMat;          // projectionMatrix
+uniform mat4 mvMat;
+uniform mat3 nMat;
+uniform mat4 pMat;
 
-uniform int ts;             // tesselation
+uniform int ts;
 uniform int waveDim;
 uniform float time;
 
-// varying = values shared with shader.frag
-varying vec3 vColor;
-
 vec3 computeLighting(vec4 rEC, vec3 nEC) {
-  // Using computeLighting() from sinewave3D-glm.cpp (modified to work)
   float shininess = 50.0;
+  // Using computeLighting() from sinewave3D-glm.cpp (modified to work)
 
   vec3 color = vec3(0.0);
+
   vec3 La = vec3(0.2);
   vec3 Ma = vec3(0.2);
   vec3 ambient = (La * Ma);
@@ -53,36 +51,36 @@ vec3 computeLighting(vec4 rEC, vec3 nEC) {
   return color;
 }
 
-void main(void)
+void calcSineYValue()
 {
-  // Calculate y values in here, x and z is calculated in cpu and passed here
-  vec4 v = gl_Vertex;
-
-  float tesselation = float(ts);
-  float stepSize = 2.0 / tesselation;
-
   const float A1 = 0.25, k1 = 2.0 * M_PI, w1 = 0.25;
   const float A2 = 0.25, k2 = 2.0 * M_PI, w2 = 0.25;
-  int i, j;
+  int i,j;
 
-  vec4 esVert, csVert;
+  float stepSize = 2.0 / float(ts);
 
   for (j = 0; j < ts; j++){
     for (i = 0; i <= ts; i++){
       if (waveDim == 2) {
-        v.y = A1 * sin(k1 * v.x + w1 * time);
+        gl_Vertex.y = A1 * sin(k1 * gl_Vertex.x + w1 * time);
       } else if (waveDim == 3) {
-        v.y = A1 * sin(k1 * v.x + w1 * time) + A2 * sin(k2 * v.z + w2 * time);
+        gl_Vertex.y = A1 * sin(k1 * gl_Vertex.x + w1 * time) + A2 * sin(k2 * gl_Vertex.z + w2 * time);
       }
-
-      //esVert = gl_ModelViewMatrix * v;
-      //csVert = gl_ProjectionMatrix * esVert;
-      esVert = mvMat * v;
-      csVert = pMat * esVert;
-      gl_Position = csVert;
-
-      vColor = gl_Color;
-      //vColor = computeLighting(esVert, nMat * normalize(gl_Normal));
     }
   }
+}
+
+void main(void)
+{
+  calcSineYValue();
+
+  // os - object space, es - eye space, cs - clip space
+  vec4 osVert = gl_Vertex;
+  vec4 esVert = mvMat * osVert;
+  vec4 csVert = pMat * esVert;
+  gl_Position = csVert;
+  // Equivalent to: gl_Position = gl_ProjectionMatrix * gl_ModelViewMatrix * gl_Vertex
+  gl_Normal = nMat * normalize(gl_Normal);
+
+  color = computeLighting(esVert, gl_Normal);
 }
---
+++ examples/7-redo/shaders.c
@@ -0,0 +1,173 @@
+/* Adapted from code originally written by pknowles */
+
+#define GL_GLEXT_PROTOTYPES
+
+#if _WIN32
+#	include <Windows.h>
+#	include <GL/glew.h>
+#endif
+#if __APPLE__
+#	include <OpenGL/gl.h>
+#	include <OpenGL/glu.h>
+#else
+#	include <GL/gl.h>
+#	include <GL/glu.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "shaders.h"
+
+#ifdef _WIN32
+#pragma warning(disable:4996)
+#endif
+
+int oglError(int line, const char* file)
+{
+  GLenum glErr;
+  int retCode = 0;
+  while ((glErr = glGetError()) != GL_NO_ERROR) {
+    /* extract file name from path */
+    const char* p = strrchr(file, '\\');
+    if (p > 0) file = p+1;
+    /* print the error message */
+    printf("glError 0x%x in %s at %i: %s\n", glErr, file, line, gluErrorString(glErr));
+    retCode = 1;
+  }
+  return retCode;
+}
+
+int shaderError(GLuint shader, const char* name)
+{
+  int infologLength = 0;
+  int charsWritten = 0;
+  int success = 0;
+  GLchar *infoLog;
+
+  CHECK_GL_ERROR;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infologLength);
+  CHECK_GL_ERROR;
+  if (!success) {
+    if (infologLength > 1) {
+      infoLog = (GLchar *)malloc(infologLength);
+      glGetShaderInfoLog(shader, infologLength, &charsWritten, infoLog);
+      printf("Shader InfoLog (%s):\n%s", name, infoLog);
+      free(infoLog);
+    }
+    else
+      printf("Shader InfoLog (%s): <no info log>\n", name);
+    return 1;
+  }
+  CHECK_GL_ERROR;
+  return 0;
+}
+
+int programError(GLuint program, const char* vert, const char* frag)
+{
+  int infologLength = 0;
+  int charsWritten = 0;
+  int success = 0;
+  GLchar *infoLog;
+
+  CHECK_GL_ERROR;
+  glGetProgramiv(program, GL_LINK_STATUS, &success);
+  glGetProgramiv(program, GL_INFO_LOG_LENGTH, &infologLength);
+  CHECK_GL_ERROR;
+  if (!success) {
+    if (infologLength > 1) {
+      infoLog = (GLchar *)malloc(infologLength);
+      glGetProgramInfoLog(program, infologLength, &charsWritten, infoLog);
+      printf("Program InfoLog (%s/%s):\n%s", vert, frag, infoLog);
+      free(infoLog);
+    }
+    else
+      printf("Program InfoLog (%s/%s): <no info log>\n", vert, frag);
+    return 1;
+  }
+  CHECK_GL_ERROR;
+  return 0;
+}
+
+char* readFile(const char* filename)
+{
+  int size;
+  char* data;
+  FILE* file = fopen(filename, "rb");
+  if (!file) return NULL;
+  fseek(file, 0, 2);
+  size = ftell(file);
+  fseek(file, 0, 0);
+  data = (char*)malloc(size+1);
+  fread(data, sizeof(char), size, file);
+  fclose(file);
+  data[size] = '\0';
+  return data;
+}
+
+void cleanupShader(GLuint vert, GLuint frag, char *vertSrc, char *fragSrc)
+{
+  glDeleteShader(vert);
+  glDeleteShader(frag);
+  free(vertSrc);
+  free(fragSrc);
+}
+
+GLuint getShader(const char* vertexFile, const char* fragmentFile)
+{
+  char* vertSrc;
+  char* fragSrc;
+
+  CHECK_GL_ERROR;
+
+  /* read the contents of the source files */
+  vertSrc = readFile(vertexFile);
+  fragSrc = readFile(fragmentFile);
+
+  /* check they exist */
+  if (!vertSrc || !fragSrc) {
+    free(vertSrc);
+    free(fragSrc);
+    printf("Error reading shaders %s & %s\n", vertexFile, fragmentFile);
+    fflush(stdout);
+    return 0;
+  }
+
+  /* create the shaders */
+  GLuint vert, frag, program;
+  vert = glCreateShader(GL_VERTEX_SHADER);
+  frag = glCreateShader(GL_FRAGMENT_SHADER);
+
+  /* pass in the source code for the shaders */
+  glShaderSource(vert, 1, (const GLchar**)&vertSrc, NULL);
+  glShaderSource(frag, 1, (const GLchar**)&fragSrc, NULL);
+
+  /* compile and check each for errors */
+  glCompileShader(vert);
+  if (shaderError(vert, vertexFile)) {
+    cleanupShader(vert, frag, vertSrc, fragSrc);
+    return 0;
+  }
+  glCompileShader(frag);
+  if (shaderError(frag, fragmentFile)) {
+    cleanupShader(vert, frag, vertSrc, fragSrc);
+    return 0;
+  }
+
+  /* create program, attach shaders, link and check for errors */
+  program = glCreateProgram();
+  glAttachShader(program, vert);
+  glAttachShader(program, frag);
+  glLinkProgram(program);
+  if (programError(program, vertexFile, fragmentFile)) {
+    cleanupShader(vert, frag, vertSrc, fragSrc);
+    glDeleteProgram(program);
+    return 0;
+  }
+  /* clean up intermediates and return the program */
+  cleanupShader(vert, frag, vertSrc, fragSrc);
+
+  return program; /* NOTE: use glDeleteProgram to free resources */
+}
